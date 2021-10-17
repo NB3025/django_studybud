c6c62e942e4e343d2a1bfb8c0656ec105b4ed919
NB <over3025@gmail.com>
2021-10-18 00:36:01 +0900

    20211018 To Database & Admin Panel

---
+++ stduybud/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+
+rooms =[
+    {'id':1, 'name':'Lets learn python!'},
+    {'id':2, 'name':'Design with me'},
+    {'id':3, 'name':'Frontend developers'},
+        ]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('base.urls'))
+]
